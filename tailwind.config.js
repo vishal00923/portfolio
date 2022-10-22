9de970d4beb31ffdf38fe07a46391a41f8cf70c1
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-10-22 17:54:28 +0530

    update : config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      xs: { max: '398px' },
+      smh: { min: '410px' },
+      sm: { min: '640px' },
+      md: { min: '768px' },
+      lg: { min: '1024px' },
+      xl: { min: '1280px' },
+      '2xl': { min: '1536px' },
+    },
   },
   plugins: [],
 };
