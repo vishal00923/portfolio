075ff4af3f268ed7660183892138754860b3fddf
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-10-23 07:21:24 +0530

    update: config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
   theme: {
     screens: {
       xs: { max: '398px' },
-      smh: { min: '410px' },
       sm: { min: '640px' },
       md: { min: '768px' },
       lg: { min: '1024px' },
       xl: { min: '1280px' },
+      xss: { max: '280px' },
+      smh: { min: '410px' },
+      smd: { min: '540px' },
       '2xl': { min: '1536px' },
     },
   },
