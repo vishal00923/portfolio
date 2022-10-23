4044de469270e46b6057d30b547efd76f73b4775
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-10-23 07:20:23 +0530

    add : utils and data

---
+++ src/constants/data.js
@@ -0,0 +1,96 @@
+export const SKILLS = [
+  {
+    id: 1,
+    name: 'HTML',
+    skill: require('../assets/html.png'),
+  },
+  {
+    id: 2,
+    name: 'CSS',
+    skill: require('../assets/css.png'),
+  },
+  {
+    id: 3,
+    name: 'ES6',
+    skill: require('../assets/js.png'),
+  },
+  {
+    id: 4,
+    name: 'REACT',
+    skill: require('../assets/react.png'),
+  },
+  {
+    id: 5,
+    name: 'NEXT',
+    skill: require('../assets/next.png'),
+  },
+  {
+    id: 6,
+    name: 'MUI',
+    skill: require('../assets/mui.png'),
+  },
+  {
+    id: 7,
+    name: 'STYLED',
+    skill: require('../assets/styled.png'),
+  },
+  {
+    id: 8,
+    name: 'TAILWIND',
+    skill: require('../assets/tailwind.png'),
+  },
+  {
+    id: 9,
+    name: 'FIREBASE',
+    skill: require('../assets/firebase.png'),
+  },
+  {
+    id: 10,
+    name: 'GITHUB',
+    skill: require('../assets/github.png'),
+  },
+  {
+    id: 11,
+    name: 'VS CODE',
+    skill: require('../assets/vscode.png'),
+  },
+];
+
+export const PROJECTS = [
+  {
+    id: 1,
+    img: require('../assets/crypto.jpg'),
+    name: 'Crypto Tracker',
+    description:
+      'Crypto Tracker is a website which tracks crypto currencies where people can add their faviourate crytp currencies in their watchlist.',
+  },
+  {
+    id: 2,
+    img: require('../assets/game.jpg'),
+    name: 'Pig Game',
+    description:
+      'Pig game is a simple dice and I created this game from scratch using the frontend tools I know.',
+  },
+  {
+    id: 3,
+    img: require('../assets/notes.jpg'),
+    name: 'Notes App',
+    description:
+      'Notes App is a website where people can add their notes and save them for later use.',
+  },
+  {
+    id: 4,
+    img: require('../assets/travel.jpg'),
+    name: 'Travel Website',
+    description:
+      'Travel website is simple website with clean UI and I created this website from scratch using the frontend tools I know.',
+  },
+];
+
+export const ABOUT_IMG = {
+  about: require('../assets/about.jpg'),
+};
+
+export const GIFY = {
+  gify: require('../assets/gify.gif'),
+};
---
+++ src/utils/helper.js
@@ -0,0 +1,3 @@
+export function toCapitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
