cb03854012c61fa9ce920c5af3e85e4ca37dbbda
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-10-25 22:23:48 +0530

    add : Main and ProjectDescription components

---
+++ src/components/Main.jsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { GIFY } from '../constants/data';
+
+export default function Main({ title, live }) {
+  const { gify } = GIFY;
+
+  return (
+    <main className='max-w-[1240px] h-screen mx-auto flex flex-col items-center justify-center px-4 mb-[172px] md:justify-center md:h-[50vh] lg:h-screen'>
+      <div className='w-[300px] smh:w-[350px] xss:w-[200px]'>
+        <Image
+          className='mix-blend-multiply'
+          src={gify}
+          alt='gify'
+          layout='responsive'
+          priority
+        />
+      </div>
+
+      <div className='text-center'>
+        <h2 className='text-4xl xss:text-3xl sm:text-5xl lg:text-6xl uppercase font-[900] text-[#4338ca] mb-6'>
+          {title}
+        </h2>
+        <p className='max-w-[520px] sm:max-w-[620px] md:max-w-[720px] lg:max-w-[920px] auto text-[1.275rem] text-center'>
+          This page contains the case study of {title} which includes the
+          Project Overview, Tools Used and Live Links to the official project.
+        </p>
+      </div>
+
+      <div className='flex justify-center mt-12 smh:mt-20 smd:mt-8'>
+        <button className='w-[200px] py-3 uppercase font-bold shadow-xl tracking-[1px] bg-[#4338ca] text-white rounded-[5px] active:scale-90 transition duration-150'>
+          <Link href={live}>
+            <a target='_blank'>go live</a>
+          </Link>
+        </button>
+      </div>
+    </main>
+  );
+}
---
+++ src/components/ProjectDescription.jsx
@@ -0,0 +1,57 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import Tools from './Tools';
+
+export default function ProjectDescription({ p1, p2, cryptoImg, live, repo }) {
+  return (
+    <section className='max-w-[1240px] m-auto mb-[52px] px-4 py-12'>
+      <div className='md:w-[75%] m-auto'>
+        <Image
+          className='mix-blend-multiply'
+          src={cryptoImg}
+          alt='crypto'
+          layout='responsive'
+          priority
+        />
+      </div>
+
+      <div className='max-w-[720px] m-auto px-2 py-14'>
+        <h3 className='text-[1.275rem] sm:text-[1.475rem] md:text-[1.5rem] lg:text-[1.625rem] pb-6'>
+          Project Overview
+        </h3>
+        <p className='pb-6 text-[1.1rem]'>{p1}</p>
+        <p className='pb-6 text-[1.1rem]'>{p2}</p>
+        <p className='text-[1.1rem]'>
+          Feel free to check out the Project by visiting the Live Link.
+        </p>
+      </div>
+
+      <div className='max-w-[720px] m-auto px-2 pb-6'>
+        <h3 className='text-[1.275rem] sm:text-[1.475rem] md:text-[1.5rem] lg:text-[1.625rem] pb-6'>
+          Tools Used
+        </h3>
+        <Tools />
+      </div>
+
+      <div className='max-w-[720px] m-auto px-2'>
+        <h2 className='text-[1.275rem] sm:text-[1.475rem] md:text-[1.5rem] lg:text-[1.625rem] pb-6'>
+          See Live
+        </h2>
+
+        <div className='flex items-center space-x-5'>
+          <button className='w-[200px] py-3 uppercase font-bold shadow-xl tracking-[1px] bg-[#4338ca] text-white rounded-[5px] active:scale-90 transition duration-150'>
+            <Link href={live}>
+              <a target='_blank'>go live</a>
+            </Link>
+          </button>
+          <button className='w-[200px] py-3 uppercase font-bold shadow-lg tracking-[1px] bg-[#ecf0f3] text-[#4338ca] border-2 border-[#4338ca] rounded-[5px] active:scale-90 transition duration-150'>
+            <Link href={repo}>
+              <a target='_blank'>github</a>
+            </Link>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
